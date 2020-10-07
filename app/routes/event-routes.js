12b07fec3f01ade8bebbbf4bc37b3fce02912423
Riadmourad <riyad.mourad@gmail.com>
2020-10-07 15:09:57 -0400

    Removed requireToken from index route
    github usernames: @milescogan92 @max-spec007 @RIADJM

--- app/routes/event-routes.js
+++ app/routes/event-routes.js
@@ -29,7 +29,7 @@ const router = express.Router()
 
 // INDEX
 // GET /examples
-router.get('/events', requireToken, (req, res, next) => {
+router.get('/events', (req, res, next) => {
   Event.find()
     .then(events => {
       // `examples` will be an array of Mongoose documents
@@ -45,7 +45,7 @@ router.get('/events', requireToken, (req, res, next) => {
 
 // SHOW
 // GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/events/:id', requireToken, (req, res, next) => {
+router.get('/events/:id', (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   Event.findById(req.params.id)
     .then(handle404)
