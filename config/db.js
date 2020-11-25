23ef717b7e41650e3a136828681cec33aaddf078
Maxwell Kwarteng <maxtheflash007@gmail.com>
2020-11-25 12:50:52 -0500

    Added edited files to Project 3

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'boot2boot-api'
+const mongooseBaseName = 'surprise-surprise-api'
 
 // create the mongodb uri for development and test
 const database = {
